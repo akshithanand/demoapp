b24769d224353c3d4fbd611c923f0e73b57d5d54
akshithanand <n.anandababu@live.com>
2021-11-21 15:52:37 -0500

    commit changes2

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php
-$servername =  "devmysqldb.mysql.database.azure.com";
-$username = "sqladmin@devmysqldb";
-$password = "Pass=1234";
-$database = "fmlyrsrc"
 
-	
-$conn=mysqli_init(); 
-mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con,$servername,$username ,$password, $database, 3306);
-
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+echo "Connected successfully new";
 
 ?>
